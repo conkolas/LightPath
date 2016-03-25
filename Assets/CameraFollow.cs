47000eb95e8024908812d4b57cf125f5525478e9
conkolas <aistismatulis@gmail.com>
2016-03-25 18:18:20 +0200

    Character controller

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 rotation;
+    public Vector3 offsetPosition;
+    public float smoothFollowTime = 0.3f;
+    public float smoothRotationTime = 0.3f;
+
+
+    public Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        Vector3 goalPos = target.position + offsetPosition;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothFollowTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotation), smoothRotationTime * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour {
+
+    [Serializable]
+    public class InputManager
+    {
+        public float verticalInput, horizontalInput;
+        public bool sprintInput;
+
+        public void Update()
+        {
+            verticalInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
+            sprintInput = Input.GetKey(KeyCode.LeftShift);
+        }
+    }
+    public InputManager inputManager;
+
+    private Animator playerAnimator;
+    private float playerVerticalDirection; //forward = 1, backward = -1
+
+	// Use this for initialization
+	void Start () {
+        playerAnimator = gameObject.GetComponent<Animator>();
+        inputManager = new InputManager();
+
+        playerVerticalDirection = 1f;
+        playerAnimator.SetFloat("Direction", playerVerticalDirection);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        inputManager.Update();
+        UpdateMovement();
+	}
+
+    private void UpdateMovement()
+    {
+        if (inputManager.verticalInput != 0)
+        {
+            if (Mathf.Sign(playerVerticalDirection) != Mathf.Sign(inputManager.verticalInput))
+            {
+                playerVerticalDirection = Mathf.Sign(inputManager.verticalInput);
+                playerAnimator.SetFloat("Direction", playerVerticalDirection);
+                playerAnimator.SetTrigger("Turn Around");
+            }
+        }
+        
+        playerAnimator.SetFloat("VSpeed", inputManager.verticalInput);
+        playerAnimator.SetFloat("HSpeed", inputManager.horizontalInput);
+
+        playerAnimator.SetBool("Run", (inputManager.verticalInput != 0) ? true : false);
+        playerAnimator.SetBool("Sprint", inputManager.sprintInput);
+
+    }
+
+}
